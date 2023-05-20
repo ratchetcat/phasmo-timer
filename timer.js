7f184e5b3fd470f4d76ba305d4583b86a813a077
ratchetcat <ratchetcat+github@gmail.com>
2023-05-20 10:54:51 -0400

    Add files via upload
    Initial commit

---
+++ timer.js
@@ -0,0 +1,58 @@
+let countdown;
+const timerDisplay = document.querySelector('#timer');
+const startButton = document.querySelector('#start');
+const resetButton = document.querySelector('#reset');
+const beep = document.querySelector('#beep');
+const startInterval = 180;
+const intervals = [
+    { seconds: 10, name: "Test", audioFile: "test.wav" },
+    { seconds: 60, name: "Demon", audioFile: "demon.wav" },
+    { seconds: 90, name: "Normal", audioFile: "normal.wav" },
+    { seconds: 170, name: "Spirit", audioFile: "spirit.wav" }
+];
+
+timerDisplay.textContent = startInterval;
+
+function timer(seconds) {
+    clearInterval(countdown);
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+
+    countdown = setInterval(() => {
+        const elapsedSeconds = Math.round((Date.now() - then) / 1000) + startInterval;
+        console.log(elapsedSeconds);
+
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+        const interval = intervals.find(i => i.seconds === elapsedSeconds);
+        if(interval) {
+            document.body.style.background = '#ff0000'; // change color
+
+            timerDisplay.textContent = interval.name;
+
+            const audio = new Audio(interval.audioFile);
+            audio.play(); // play sound
+
+            setTimeout(() => document.body.style.background = '', 1000); // revert color after 1 sec
+        }
+
+        if(secondsLeft < 0) {
+            clearInterval(countdown);
+            return;
+        }
+
+        displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    timerDisplay.textContent = seconds;
+}
+
+startButton.addEventListener('click', () => timer(startInterval));
+resetButton.addEventListener('click', () => {
+    clearInterval(countdown);
+    timerDisplay.textContent = startInterval;
+});
