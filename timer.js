a68505572580977ec3b316f6e6f0525f5059ff8d
jfuller <jfuller@erg7.com>
2023-05-21 09:37:54 -0400

    Add cross-browser AudioContext

--- timer.js
+++ timer.js
@@ -1,15 +1,83 @@
+// utility code to set up cross-browser AudioContext
+// from https://curtisrobinson.medium.com/how-to-auto-play-audio-in-safari-with-javascript-21d50b0a2765
+let audioContext = new (window.AudioContext || window.webkitAudioContext)()
+function loadSound(filename) {
+    let sound = {volume: 1, audioBuffer: null}
+
+    let ajax = new XMLHttpRequest()
+    ajax.open("GET", filename, true)
+    ajax.responseType = "arraybuffer"
+    ajax.onload = function() {
+        audioContext.decodeAudioData
+        (
+            ajax.response,
+            function(buffer) {
+                sound.audioBuffer = buffer
+            },
+            function(error) {
+                debugger
+            }
+        )
+    }
+
+    ajax.onerror = function() {
+        debugger
+    }
+
+    ajax.send()
+
+    return sound
+}
+function playSound(sound) {
+    if(!sound.audioBuffer)
+        return false
+
+    let source = audioContext.createBufferSource()
+    if(!source)
+        return false
+
+    source.buffer = sound.audioBuffer
+    if(!source.start)
+        source.start = source.noteOn
+
+    if(!source.start)
+        return false
+    let gainNode = audioContext.createGain()
+    gainNode.gain.value = sound.volume
+    source.connect(gainNode)
+    gainNode.connect(audioContext.destination)
+
+    source.start(0)
+
+    sound.gainNode = gainNode
+    return true
+}
+function stopSound(sound) {
+    if(sound.gainNode)
+        sound.gainNode.gain.value = 0
+}
+function setSoundVolume(sound, volume) {
+    sound.volume = volume
+
+    if(sound.gainNode)
+        sound.gainNode.gain.value = volume
+}
+
 let counter;
 const timerDisplay = document.querySelector('#timer');
 const startButton = document.querySelector('#start');
 const resetButton = document.querySelector('#reset');
-const beep = document.querySelector('#beep');
 const targetInterval = 180;
 const intervals = [
-    { seconds: 10, name: "Test", audioFile: "test.wav" },
-    { seconds: 60, name: "Demon", audioFile: "demon.wav" },
-    { seconds: 90, name: "Normal", audioFile: "normal.wav" },
-    { seconds: 170, name: "Spirit", audioFile: "spirit.wav" }
+    { seconds: 10, name: "Test", audioFile: loadSound("test.wav") },
+    { seconds: 60, name: "Demon", audioFile: loadSound("demon.wav") },
+    { seconds: 90, name: "Normal", audioFile: loadSound("normal.wav") },
+    { seconds: 170, name: "Spirit", audioFile: loadSound("spirit.wav") }
 ];
+
+// set unlockSound and volume
+const unlockSound = loadSound("test.wav");
+unlockSound.volume = 0;
 
 // set initial count
 displayCount(0);
@@ -33,11 +101,7 @@ function timer() {
             // display interval name
             displayCount(interval.name);
 
-            // play audio
-            const audio = new Audio(interval.audioFile);
-            audio.muted = true;
-            audio.muted = false;
-            audio.play(); // play sound
+            playSound(interval.audioFile);
 
             setTimeout(() => {
                 document.body.style.background = ''
@@ -55,13 +119,9 @@ function displayCount(seconds) {
     timerDisplay.textContent = seconds;
 }
 
+// startButton unlocks AudioContext and starts timer
 startButton.addEventListener('click', () => {
-    // Play a silent audio to unlock audio playback
-    const unlockAudio = new Audio();
-    unlockAudio.muted = true;
-    unlockAudio.muted = false;
-    unlockAudio.play();
-
+    playSound(unlockSound)
     timer(0);
 });
 
